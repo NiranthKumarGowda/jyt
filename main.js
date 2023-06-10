5ec5404b94ebe8e740545326be7a8e2490193f67
NiranthKumarGowda <120294683+NiranthKumarGowda@users.noreply.github.com>
2023-06-10 09:14:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var speechrecognition=window.webkitSpeechRecognition;
+var recognition=new speechrecognition();
+function start(){
+    document.getElementById("textbox").innerHTML=""
+    recognition.start()
+}
+recognition.onresult=function(event){
+    console.log(event)
+    var content=event.results[0][0].transcript
+    document.getElementById("textbox").innerHTML=content
+    speak()
+}
+function speak(){
+ var synth=window.speechSynthesis
+ speakdata=document.getElementById("textbox").value
+ var utterThis=new SpeechSynthesisUtterance(speakdata)
+ synth.speak(utterThis)
+ Webcam.attach(camera)
+}
+Webcam.set({
+    width:500,height:400,image_format:"png",png_quality:90
+    })
+    camera=document.getElementById("camera")
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+      document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="selfie_image">'
+    }
+    (function(data_uri){
+    document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="selfie_image">'
+    }
+    (function(data_uri){
+    document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="selfie_image">'
+    }
